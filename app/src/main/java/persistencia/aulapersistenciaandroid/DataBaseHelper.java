e38be62d92bffade6575a69216a8ab5d16c83acc
sandeco <sandecom@gmail.com>
2015-04-22 22:11:40 -0300

    Initial commit

---
+++ app/src/main/java/persistencia/aulapersistenciaandroid/DataBaseHelper.java
@@ -0,0 +1,34 @@
+package persistencia.aulapersistenciaandroid;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by sanderson on 22/04/2015.
+ */
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    private static final String BANCO_DADOS = "MeuBanco";
+    private static int VERSAO = 1;
+
+    public DataBaseHelper(Context context){
+        super(context,BANCO_DADOS,null,VERSAO);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String ddl = "CREATE TABLE pessoa(" +
+                     "_id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                     "nome TEXT not null"+
+                     ")";
+        db.execSQL(ddl);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
